e61aac77870062d839210b558ddd8d5f58468ced
JonThom <jonatan.thompson@gmail.com>
2019-11-24 12:47:09 +0100

    t.test greater rather than two.sided

--- code/wgcna.R
+++ code/wgcna.R
@@ -3394,9 +3394,11 @@ if (resume == "checkpoint_2") {
             if (length(unique(colors))>1){
               vec_t <- ifelse(colors!="grey", (pkMs*sqrt(length(pkMs)-2))/sqrt(1-pkMs^2), 0) # compute t.stats, for a vector of rho
               vec_p.val <- ifelse(colors!="grey", 
-                                  stats::pt(q=abs(vec_t),  
+                                  stats::pt(q=vec_t,#abs(vec_t),  
                                   lower.tail = F, 
-                                  df = length(pkMs)-2) + stats::pt(q=-(abs(vec_t)),  lower.tail = T, df = length(pkMs)-2), 1) # compute p.values. We are using both tails
+                                  df = length(pkMs)-2),1) #+ stats::pt(q=-(abs(vec_t)),  
+                                                       #            lower.tail = T, 
+                                                        #           df = length(pkMs)-2), 1) # compute p.values. We are using both tails
           
               vec_q.val <- p.adjust(p = vec_p.val, method = "fdr")
               vec_idx_signif <- vec_q.val < pvalThreshold # compute boolean significance vector. Set "grey" to TRUE so we don't count them
@@ -3474,7 +3476,7 @@ if (resume == "checkpoint_2") {
               
               for (color in names(table(colors))[!grepl("^grey$",names(table(colors)))]) {
                 vec_p.val[colors==color] <- apply(X = datExpr[,colors==color], MARGIN = 2, FUN = function(datExpr_col) cor.test(datExpr_col, embed_mat[, color,drop=F], 
-                                                                                                                                alternative = "two.sided", 
+                                                                                                                                alternative = "greater",#"two.sided", 
                                                                                                                                 method = "pearson", 
                                                                                                                                 conf.level = 1-pvalThreshold)$p.value)
               }
